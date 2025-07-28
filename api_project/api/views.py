de7e1a107888140ec31126780e2b2cf7b1f05914
Blackburn0 <hamzatadebayo5@gmail.com>
2025-07-28 18:22:12 +0100

    api logic for book list

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets.ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 
