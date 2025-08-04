1c166d1f7dc3da2a001e361944625b6d5b29e4fc
Blackburn0 <hamzatadebayo5@gmail.com>
2025-08-04 09:18:06 +0100

    views for book logic

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -13,7 +13,7 @@ from django_filters import rest_framework
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [filters.OrderingFilter, SearchFilter, OrderingFilter]
     filterset_fields = ['title', 'author__name', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
