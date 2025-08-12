47f56ec85b4e7bfa3e7c99fd5b937bf480fe73c9
Blackburn0 <hamzatadebayo5@gmail.com>
2025-08-12 17:08:41 +0100

    initial project setup

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from .models import Post
+
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -8,3 +11,19 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "content"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Post title"}),
+            "content": forms.Textarea(attrs={"class": "form-control", "rows": 8, "placeholder": "Write your post..."}),
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title", "").strip()
+        if not title:
+            raise forms.ValidationError("Title cannot be empty.")
+        return title
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,10 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from .models import Post
+from .forms import PostForm
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
@@ -34,4 +41,45 @@ def logout_view(request):
 def profile(request):
     return render(request, 'blog/profile.html', {'user': request.user})from django.shortcuts import render
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"  # blog/templates/blog/post_list.html
+    context_object_name = "posts"
+    paginate_by = 10  # optional
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+    login_url = "login"   # adjust if your login URL is different
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+    login_url = "login"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("blog:post-list")
+    login_url = "login"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
